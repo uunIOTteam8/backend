2e8a7da9ce92db682928f904434e91d684420e31
ZuzanaValkounova <zuzana.valkounova@seznam.cz>
2024-04-01 18:40:43 +0200

    delete, update, get medicine

--- controller/medicine-controller.js
+++ controller/medicine-controller.js
@@ -2,7 +2,13 @@ const express = require("express");
 const router = express.Router();
 
 // ABL
-const { createMedicineAbl } = require("../abl/medicine-abl");
+const {
+	createMedicineAbl,
+	getMedicineAbl,
+	getMedicineByMedsTakerAbl,
+	deleteMedicineAbl,
+	updateMedicineAbl,
+} = require("../abl/medicine-abl");
 
 // Middleware
 const validate = require("../middlewares/validation-middleware");
@@ -12,13 +18,24 @@ const { validateToken } = require("../utils/JWT");
 const { medicineSchema } = require("../validators/medicine-validator");
 
 // Routes
-router.post(
-  "/create",
-  validateToken,
-  validate(medicineSchema),
-  async (req, res) => {
-    await createMedicineAbl(req, res);
-  }
-);
+router.post("/create", validateToken, validate(medicineSchema), async (req, res) => {
+	await createMedicineAbl(req, res);
+});
+
+router.get("/get/:id", validateToken, async (req, res) => {
+	await getMedicineAbl(req, res);
+});
+
+router.get("/getByMedsTaker/:medsTakerId", validateToken, async (req, res) => {
+	await getMedicineByMedsTakerAbl(req, res);
+});
+
+router.delete("/delete/:id", validateToken, async (req, res) => {
+	await deleteMedicineAbl(req, res);
+});
+
+router.patch("/update/:id", validateToken, async (req, res) => {
+	await updateMedicineAbl(req, res);
+});
 
 module.exports = router;
